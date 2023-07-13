cfb40fa70a60c7197405275d214fd6f4c8534de6
eliram9 <eliram9@gmail.com>
2023-07-13 09:59:12 -0400

    icons and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FiSun, FiMoon } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
 import { Logo } from '../media/icons'
+
+const CustomLink = ({ href, title, className = "" }) => {
+    let location = useLocation()
+    console.log(location.pathname)
+    return (
+        <a href={href} className={`${className} relative group`}>
+            {title}
+            <span className={`absolute h-[1px] inline-block bg-teal-900 left-0 -bottom-0.5
+                            group-hover:w-full transition-[width] ease duration-800"
+                            ${location.pathname === href ? "w-full" : "w-0"} dark:bg-light`}
+            >
+                &nbsp;
+            </span>
+        </a>
+    );
+}
 
 const Navbar = () => {
   const [isDarkMode, setIsDarkMode] = React.useState(false);
+  
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
     // Add dark mode logic here
   };
+
+  
 
     return (
         <nav className="flex items-center justify-between px-6 py-6 poppins font-light border-b-[3px]">
@@ -19,18 +38,10 @@ const Navbar = () => {
                     <span className="text-[26px] text-emerald-900 font-light ml-4">Generations</span>
                 </Link>    
                 <div className="ml-10">
-                    <a href="/our-story" className="ml-10 text-emerald-900 font-normal hover:text-emerald-500">
-                        Our Story
-                    </a>
-                    <a href="/explanation" className="ml-10 text-emerald-900 font-normal hover:text-emerald-500">
-                        Explanation
-                    </a>
-                    <a href="/resources" className="ml-10 text-emerald-900 font-normal hover:text-emerald-500">
-                        Resources
-                    </a>
-                    <a href="/contact-us" className="ml-10 text-emerald-900 font-normal hover:text-emerald-500">
-                        Contact Us
-                    </a>
+                    <CustomLink href="/our-story" title="Our Story" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
+                    <CustomLink href="/explanation" title="Explanation" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
+                    <CustomLink href="/resources" title="Resources" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
+                    <CustomLink href="/contact-us" title="Contact Us" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
                 </div>
             </div>
             <div className="flex items-center">
