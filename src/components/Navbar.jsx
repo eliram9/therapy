da4c2cd679c77b39d86d7e8c256239dc52f51284
eliram9 <eliram9@gmail.com>
2023-07-23 00:52:39 -0400

    navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,17 +77,6 @@ const Navbar = () => {
                             <p className='font-extralight dark:text-slate-900 dark:font-light text-sm'>1-(234)-567-8901</p>
                         </div>
                     </nav>
-        <nav className="flex items-center justify-between px-6 py-6 poppins font-light border-b-[3px] dark:bg-gray-500">
-            <div className="flex items-center">
-                <Link className='flex' to="/">
-                    <Logo className='w-10' />
-                    <span className="text-[26px] text-emerald-900 font-light ml-4">Generations</span>
-                </Link>    
-                <div className="ml-10">
-                    <CustomLink href="/our-story" title="Our Story" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
-                    <CustomLink href="/explanation" title="Explanation" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
-                    <CustomLink href="/resources" title="Resources" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
-                    <CustomLink href="/contact-us" title="Contact Us" className="ml-10 text-emerald-900 font-normal hover:text-teal-600" />
                 </div>
                 : 
                 null
