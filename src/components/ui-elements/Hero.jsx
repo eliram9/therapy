88507a16d373f6ed294d0cbf194ed37f640ccc6b
eliram9 <eliram9@gmail.com>
2024-01-29 09:38:45 -0500

    Hero comp. footer style change

---
+++ src/components/ui-elements/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { HeroBackground } from '../../media/icons';
+
+const Hero = () => {
+
+    return (
+        <section>
+            
+            <div className="w-full h-64"1985
+            >
+            <HeroBackground />
+            </div>
+        </section>
+    )
+}
+
+export default Hero
